9fda754ad492b22881025205c804b0c6ae9d8f31
Fran Gette <franngette@gmail.com>
2020-11-18 00:00:50 -0300

    Added screen content, styles.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+  Platform,
+} from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoriesScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go To Meals"
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
         onPress={() => {
-          props.navigation.navigate({routeName: 'CategoryMeals'});
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
         }}
-      />
-    </View>
+        style={styles.gridItem}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +49,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = (props) => {
+
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go To Details"
         onPress={() => {
           props.navigation.navigate({ routeName: "MealDetail" });
         }}
       />
-      <Button title="Go Back" onPress={() => {
+      <Button
+        title="Go Back"
+        onPress={() => {
           props.navigation.goBack();
-      }} />
+        }}
+      />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
 };
 
 const styles = StyleSheet.create({
